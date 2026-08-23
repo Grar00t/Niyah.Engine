784bdab0ad3c11fa5e6aa91503b38a349999ba45
SULIMAN ALSHAMMARI <239743497+Grar00t@users.noreply.github.com>
2026-08-23 11:03:49 -0700

    Simplify niyah_telemetry.h

--- native/niyah_telemetry.h
+++ native/niyah_telemetry.h
@@ -1,50 +1,9 @@
 #ifndef NIYAH_TELEMETRY_H
 #define NIYAH_TELEMETRY_H
 
-#include <stdbool.h>
-#include <stdint.h>
+#include "niyah.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+void niyah_telemetry_start(NiyahTelemetry* telemetry);
+void niyah_telemetry_end(NiyahTelemetry* telemetry);
 
-typedef struct {
-    bool enabled;
-    bool include_content; /* reserved; ignored by current privacy contract */
-    const char *path;
-} NiyahTelemetryConfig;
-
-typedef struct {
-    uint64_t started_unix_ms;
-    uint64_t events;
-    uint64_t errors;
-    uint64_t bytes_in;
-    uint64_t bytes_out;
-} NiyahTelemetryStats;
-
-typedef struct {
-    NiyahTelemetryConfig config;
-    NiyahTelemetryStats stats;
-    uint64_t started_monotonic_ns;
-} NiyahTelemetry;
-
-bool niyah_telemetry_init(NiyahTelemetry *telemetry,
-                          const NiyahTelemetryConfig *config,
-                          uint64_t now_unix_ms,
-                          uint64_t now_monotonic_ns);
-void niyah_telemetry_close(NiyahTelemetry *telemetry);
-
-bool niyah_telemetry_event(NiyahTelemetry *telemetry,
-                           const char *name,
-                           uint64_t duration_ms,
-                           uint64_t bytes_in,
-                           uint64_t bytes_out,
-                           bool error);
-
-const NiyahTelemetryStats *niyah_telemetry_stats(const NiyahTelemetry *telemetry);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#endif // NIYAH_TELEMETRY_H
